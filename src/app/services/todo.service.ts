77a3152ffbadd6b1612ba7fa89a727b9c9d02f4b
Doli jodhani <16mscit091@gmail.com>
2020-07-28 10:19:14 -0700

    first commit

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../component/models/Todo';
+
+const httpOptions= {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  toodsLimit= '?_limit=5';
+  constructor(private http:HttpClient) { }
+
+  getTodos():Observable<Todo[]>{
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.toodsLimit}`);
+    // return [{
+    //   id:1,
+    //   title:'Todo one',
+    //   completed:false
+    // },
+    // {
+    //   id:2,
+    //   title:'Todo two',
+    //   completed:false
+    // },
+    // {
+    //   id:3,
+    //   title:'Todo three',
+    //   completed:true
+    // },]
+  }
+  addTodo(todo : Todo):Observable<Todo>{
+    return this.http.post<Todo>(this.todosUrl,todo,httpOptions);
+  }
+
+  deleteTodo(todo : Todo):Observable<Todo>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url,httpOptions);
+  }
+
+  toggleCompleted(todo : Todo):Observable<any>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url,todo,httpOptions);
+  }
+}
+
